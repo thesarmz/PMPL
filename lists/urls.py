35a6aa95518d9fd883ff7ccfa2a1d956ae2bbdf5
Thesar Muhammad Zuhri <tesarm.zuhri@gmail.com>
2015-11-17 11:35:05 +0700

    Use get_absolute_url on List model to DRY urls in views

--- lists/urls.py
+++ lists/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^(\d+)/$', views.view_list, name='view_list'),
 ]
 
+
 #urlpatterns = [
 #    url(r'^new$', views.new_list, name='new_list'),
 #    url(r'^(\d+)/$', views.view_list, name='view_list'),
--- lists/views.py
+++ lists/views.py
@@ -16,20 +16,18 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    
-    return render(request, 'list.html', {'list': list_, 'error': error})
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
     error = None
 
     if request.method == 'POST':
         try:
             item = Item(text=request.POST['item_text'], list=list_)
-            item.full_clean()
+            item.full_clean()     
             item.save()
-            return redirect('/lists/%d/' % (list_.id,))
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
+
+            
 
     comment = ''
     countlist = Item.objects.filter(list_id=list_.id).count()
@@ -52,7 +50,8 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect('/lists/%d/' % (list_.id,))
+    return redirect(list_)
+    #return redirect('view_list', list_.id)
 
 #def add_item(request, list_id):
  #   list_ = List.objects.get(id=list_id)
