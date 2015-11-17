2343c079a2edc684f2f75880aa58bbcdff469ec4
Thesar Muhammad Zuhri <tesarm.zuhri@gmail.com>
2015-11-17 11:17:57 +0700

    Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -14,9 +14,11 @@ def home_page(request):
     items = Item.objects.all()
     return render(request, 'home.html', {'comment': comment})
 
-	
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
 
     comment = ''
     countlist = Item.objects.filter(list_id=list_.id).count()
@@ -29,11 +31,6 @@ def view_list(request, list_id):
 
     return render(request, 'list.html', {'list': list_, 'comment':comment})
 	
-#def new_list(request):
- #   list_ = List.objects.create()
-  #  item = Item.objects.create(text=request.POST['item_text'], list=list_)
-   # item.full_clean()
-    #return redirect('/lists/%d/' % (list_.id,))
 def new_list(request):
     list_ = List.objects.create()
     item = Item(text=request.POST['item_text'], list=list_)
@@ -46,8 +43,8 @@ def new_list(request):
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
+#def add_item(request, list_id):
+ #   list_ = List.objects.get(id=list_id)
+  #  Item.objects.create(text=request.POST['item_text'], list=list_)
+   # return redirect('/lists/%d/' % (list_.id,))
     
