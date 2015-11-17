5565c6e141083d8e949b771a43d0cbffbe452452
Thesar Muhammad Zuhri <tesarm.zuhri@gmail.com>
2015-11-17 10:43:25 +0700

    Adjust new list view to do model validation

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect,render
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
+
 
 def home_page(request):
 #    if request.method == 'POST':      
@@ -27,11 +29,23 @@ def view_list(request, list_id):
 
     return render(request, 'list.html', {'list': list_, 'comment':comment})
 	
+#def new_list(request):
+ #   list_ = List.objects.create()
+  #  item = Item.objects.create(text=request.POST['item_text'], list=list_)
+   # item.full_clean()
+    #return redirect('/lists/%d/' % (list_.id,))
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
-	
+
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
